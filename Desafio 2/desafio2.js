18b91fa023d31253317575316b6aa499b9c0a0ef
jonathan-ezequiel-cepeda <jonathan.e.cepeda@gmail.com>
2021-09-16 23:29:41 -0300

    se cambio la cantidad posible de numeros

--- Desafio 2/desafio2.js
+++ Desafio 2/desafio2.js
@@ -1,10 +1,10 @@
 //Desafio 2 - Crea un algoritmo con un condicional
 
-let numeroIngresado = prompt("ingrese aqui un numero del 1 al 10")
+let numeroIngresado = prompt("ingrese aqui un numero del 1 al 100")
 
-if ((numeroIngresado < 1) || (numeroIngresado > 10)) {
+if ((numeroIngresado < 1) || (numeroIngresado > 100)) {
   alert("Ese no es un numero entre 1 y 10. Media pila.")
 }
-else if ((numeroIngresado >= 1) || (numeroIngresado <=10 ) ){
+else if ((numeroIngresado >= 1) || (numeroIngresado <=100 ) ){
   alert("ingresaste " + numeroIngresado + ". Gracias!")
 }
