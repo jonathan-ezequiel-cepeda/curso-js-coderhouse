e2c75560f662e4b6477e922eecf72cb473e7d836
jonathan-ezequiel-cepeda <jonathan.e.cepeda@gmail.com>
2021-10-28 05:40:09 -0300

    se arreglaron unas rutas del index que estaban mal

--- 3era Entrega Del Proyecto Final/proyecto-final/js/index.js
+++ 3era Entrega Del Proyecto Final/proyecto-final/js/index.js
@@ -1,4 +1,4 @@
-$(".btnPetalo").prepend('<button><img class="petalo-btn" src="../imagenes/sakura-btn.png" alt="sakura10"></button>')
+$(".btnPetalo").prepend('<button><img class="petalo-btn" src="./imagenes/sakura-btn.png" alt="sakura10"></button>')
 
 $(".petalo-btn").on("click", () => {
   $(".petalo").slideToggle()
--- Desafio 13 (Efectos y animaciones con jQuery)/proyecto-final/js/index.js
+++ Desafio 13 (Efectos y animaciones con jQuery)/proyecto-final/js/index.js
@@ -1,5 +1,12 @@
-$(".btnPetalo").prepend('<button><img class="petalo-btn" src="../imagenes/sakura-btn.png" alt="sakura10"></button>')
+$(".btnPetalo").prepend('<button><img class="petalo-btn" src="./imagenes/sakura-btn.png" alt="sakura10"></button>')
 
 $(".petalo-btn").on("click", () => {
   $(".petalo").slideToggle()
 })
+
+$(".petalo-btn").slideUp(1000)
+.slideDown(1000)
+.slideUp(2000)
+.slideDown(2000)
+.slideUp(3000)
+.slideDown(3000);
